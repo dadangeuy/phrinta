e5bf2b4372e38208fa9cbccfa596d12c614dfa27
dadangeuy <irsyad.rizaldi97@gmail.com>
2018-10-18 09:58:23 +0700

    Initial Project

---
+++ src/main/java/com/rizaldi/phrinta/PhrintaApplication.java
@@ -0,0 +1,12 @@
+package com.rizaldi.phrinta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrintaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhrintaApplication.class, args);
+    }
+}
