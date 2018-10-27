9124f6fa847e950baf5eedd51979f17bf410c690
dadangeuy <irsyad.rizaldi97@gmail.com>
2018-10-27 14:20:38 +0700

    secure admin page

--- src/main/java/com/rizaldi/phrinta/config/SecurityConfig.java
+++ src/main/java/com/rizaldi/phrinta/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.rizaldi.phrinta.config;
 
+import com.rizaldi.phrinta.model.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String ADMIN_AUTH = User.Role.ADMIN.toGrantedAuthority().getAuthority();
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,6 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/user/register").permitAll()
+                .antMatchers("/admin/**", "/api/admin/**").hasAuthority(ADMIN_AUTH)
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll()
